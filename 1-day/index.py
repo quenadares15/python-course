828e6c39dd5e91b0cbdc6255277494ed20477d6f
quenadares15 <quenadares.developer@gmail.com>
2021-01-20 12:44:01 +0100

    Added 1 day

---
+++ 1-day/index.py
@@ -0,0 +1,32 @@
+# 1 DAY
+
+# Show text in console
+print("\nHello World")
+input("\nPress Enter to continue\n")
+# Use print() to show text in console
+# Before the program exits, the user must press the enter key - input()
+# If you want to get empty line in console, write \n in string
+
+# Define variable
+variable1 = "string"
+variable2 = 123 #number
+# To define variable write: 
+# name-of-variable = "string" / 123 
+
+# Showing variable in console
+a = "variable a"
+b = "variable b"
+print(a)
+print(b)
+# Showing content of variable
+input("\nPress Enter to continue\n")
+
+# Getting value from the user
+name = input("What's your name?: ")
+print("Hello " + name)
+input("Press Enter to exit")
+# First, you need to define variable
+# Next, you need to use print() to show content of variable
+# Use + to connect strings
+
+# Congratulations, 1 day complete!
