bf39c5118b33e0aefbd07026851ed027fa62d632
quenadares15 <quenadares.developer@gmail.com>
2021-01-20 13:12:14 +0100

    Added 3 day

---
+++ 3-day/calculator.py
@@ -0,0 +1,52 @@
+# 3 DAY
+
+# Operations on numbers
+print("\nAddition\n")
+a = input("First Nmber: ")
+b = input("Second Number: ")
+result = float(a) + float(b)
+print("Result: " + str(result))
+
+input("\nPress Enter to continue\n")
+
+print("Subtraction\n")
+a1 = input("First Nmber: ")
+b1 = input("Second Number: ")
+result1 = float(a1) - float(b1)
+print("Result: " + str(result1))
+
+input("\nPress Enter to continue\n")
+
+print("Increase\n")
+a2 = input("First Nmber: ")
+b2 = input("Second Number: ")
+result2 = float(a2) * float(b2)
+print("Result: " + str(result2))
+
+input("\nPress Enter to continue\n")
+
+print("Split without rest\n")
+a3 = input("First Nmber: ")
+b3 = input("Second Number: ")
+result3 = int(a3) // int(b3)
+print("Result: " + str(result3))
+
+input("\nPress Enter to continue\n")
+
+print("Split with rest\n")
+a4 = input("First Nmber: ")
+b4 = input("Second Number: ")
+result4 = int(a4) / int(b4)
+print("Result: " + str(result4))
+
+input("\nPress Enter to continue\n")
+
+print("Rest of the split\n")
+a5 = input("First Nmber: ")
+b5 = input("Second Number: ")
+result5 = int(a5) % int(b5)
+print("Result: " + str(result5))
+
+input("\nPress Enter to continue\n")
+
+
