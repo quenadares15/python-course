f104eb7927b78ac3cb91721c7e2c48477ab3582f
quenadares15 <quenadares.developer@gmail.com>
2021-01-20 12:48:52 +0100

    Added 2 day

---
+++ 2-day/index.py
@@ -0,0 +1,36 @@
+# 2 DAY
+
+# Data types:
+#BOOLEAN (True, False)
+print("\nData types:\n")
+a = True
+b = False
+print(a)
+print(b)
+input("\nPress Enter to continue\n")
+
+# INTEGER int()
+first = input("First Number: ")
+second = input("Second Number: ")
+print(int(first) + int(second))
+input("\nPress Enter to continue\n")
+# int() convert string to integer
+
+# FLOATING POINT NUMBER float()
+float1 = input("First Number: ")
+float2 = input("Second Number: ")
+print(float(float1) + float(float2))
+input("\nPress Enter to continue\n")
+# In float() you can add two number with pointer
+# For example, 2.2 + 2.2 give result 4.4 
+
+# STRING str()
+str1 = input("First Number / Word: ")
+str2 = input("Second Number / Word: ")
+print(str(str1) + str(str2))
+input("\nType Enter to exit\n")
+# str() is used to connecting strings together
+# For example, in str() 2+2 give result 22 (not 4)
+# In int(), 2+2 give result 4
+
+# Congratulations, 2 day complete!
