ff85797dacd8e74288c676aa27e9979d7b22b3b4
quenadares15 <quenadares.developer@gmail.com>
2021-01-22 20:24:17 +0100

    Added 6 day

---
+++ 6-day/index.py
@@ -0,0 +1,21 @@
+# 6 DAY (instrukcje warunkowe)
+
+# Conditional statements is needed to making conditions
+# For example, if playernick == blacklist: print(playernick + "is trying to join the game, but he is on backlisted")
+# You can make conditions to check lenght of the playernick and print that this isn't allowed lenght of nick
+
+# I start by making variables
+a = 5
+b = 5
+
+# Here is conditional statement
+if a == b:
+    print("a is equal to b")
+elif a >= b:
+    print("a is greater than or equal to b")
+elif a <= b:
+    print("a is smaller than or equal to b")
+else:
+    print("a is not equal to b")
+
+# 6th day complete!
