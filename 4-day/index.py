6ab6ff18b9b53e6c3eadcca485294952275f4b22
dotandl <oskarzawidlak@gmail.com>
2021-01-22 19:18:56 +0100

    Add 4th day

---
+++ 4-day/index.py
@@ -0,0 +1,67 @@
+# 4TH DAY
+# Comparison operators
+
+# All of the comparison operators returns a bool. It means the result of the
+# comparison will be either True or False.
+
+# Equality operator
+# a == b
+print('EQUALITY')
+
+equality = 2 == 2 # Will be True, because 2 is equal 2
+print('2 == 2 -> ' + str(equality))
+
+equality = 3 == 5 # Will be False, because 3 is NOT equal 5
+print('3 == 5 -> ' + str(equality))
+
+# Inequality operator
+# a != b
+print('INEQUALITY')
+
+inequality = 10 != 20 # Will be True, because 10 is not equal 20
+print('10 != 20 -> ' + str(inequality))
+
+inequality = 15 != 15 # Will be False, because 15 IS equal 15
+print('15 != 15 -> ' + str(inequality))
+
+# Greater than operator
+# a > b
+print('GREATER THAN')
+
+greater_than = 15 > 10 # Will be True, because 15 is greater than 10
+print('15 > 10 -> ' + str(greater_than))
+
+greater_than = 15 > 15 # Will be False, because 15 is NOT greater than 15
+print('15 > 15 -> ' + str(greater_than))
+
+# Less than operator
+# a < b
+print('LESS THAN')
+
+less_than = 5 < 10 # Will be True, because 5 is less than 10
+print('5 < 10 -> ' + str(less_than))
+
+less_than = 100 < 15 # Will be False, because 100 is NOT less than 15
+print('100 < 15 -> ' + str(less_than))
+
+# Greater than or equal operator
+# a >= b
+print('GREATER THAN OR EQUAL')
+
+greater_than_or_equal = 15 >= 10 # Will be True, because 15 is greater than 10
+print('15 >= 10 -> ' + str(greater_than_or_equal))
+
+greater_than_or_equal = 10 >= 20 # Will be False, because 20 is neither greater than nor equal 10
+print('10 >= 20 -> ' + str(greater_than_or_equal))
+
+# Less than or equal operator
+# a < b
+print('LESS THAN OR EQUAL')
+
+less_than_or_equal = 10 <= 10 # Will be True, because 10 is equal 10
+print('10 <= 10 -> ' + str(less_than_or_equal))
+
+less_than_or_equal = 20 <= 3 # Will be False, because 20 is neither less than nor equal 3
+print('20 <= 3 -> ' + str(less_than_or_equal))
+
+# Congratulations, 4th day complete!
