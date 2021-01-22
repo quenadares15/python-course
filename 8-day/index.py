0f086095859998b48998e08ebe8a48846c0aaf75
quenadares15 <quenadares.developer@gmail.com>
2021-01-22 22:24:31 +0100

    Added 8 day

---
+++ 8-day/index.py
@@ -0,0 +1,22 @@
+# 8 DAY
+
+# Break and continue is needed to make for example counter or making of the game
+
+# I started by creating variable i and j
+# Then making while loop that if i variable will greater than 6, print i
+# And i is greater or equal than 1
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+j = 0
+while i < 6:
+  j += 1
+  if j == 3:
+    continue
+  print(i)
+
+# 8 day complete!
